29dc0338438438e1e706ed5a57224b83cd896e2a
Maximiliano Ovejak <movejak@ga.com.mx>
2025-07-08 13:35:45 -0500

    Pagina 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button, Container, Text } from '@mantine/core'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <Container fluid>
+      <Text size="xl" fw={700}>Direccion erronea o en contrucción</Text>
+      <Text>Todavía no arranco esto, banquen un poco</Text>
+      <Button 
+        component={Link}
+        href={"/"}
+      >
+        Volver a Inicio
+      </Button>
+    </Container>
+  )
+}
